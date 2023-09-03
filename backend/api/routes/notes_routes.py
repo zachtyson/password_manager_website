bee28f64d50e5bb90196dfde6c091da09f379ba5
Zachary <tysonzachary@ufl.edu>
2023-09-03 16:56:50 -0400

    get, post, delete

--- backend/api/routes/notes_routes.py
+++ backend/api/routes/notes_routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from db.models import Note
 from db.session import SessionLocal
+from db.schemas import NoteCreate, NoteInDB, NoteBase, ItemBase, ItemCreate, ItemInDB
+from typing import List
 
 router = APIRouter()
 
@@ -14,6 +16,26 @@ def get_db():
         db.close()
 
 
-@router.get("/notes/")
+@router.get("/notes/", response_model=List[NoteInDB])
 def get_notes(db: Session = Depends(get_db)):
-    return db.query(Note).all()
+    notes = db.query(Note).all()
+    return notes
+
+
+@router.post("/notes/", response_model=NoteCreate)
+def create_note(note: NoteCreate, db: Session = Depends(get_db)):
+    new_note = Note(title=note.title, content=note.content)
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    return new_note
+
+
+@router.delete("/notes/{note_id}", response_model=NoteInDB)
+def delete_note_by_id(note_id: int, db: Session = Depends(get_db)):
+    note = db.query(Note).filter(Note.id == note_id).first()
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    db.delete(note)
+    db.commit()
+    return note
