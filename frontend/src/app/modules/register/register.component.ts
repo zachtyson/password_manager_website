033661d9981ffccfc00f3088115759b4ab68128f
Zachary <tysonzachary@ufl.edu>
2023-09-04 17:46:01 -0400

    submit user

--- frontend/src/app/modules/register/register.component.ts
+++ frontend/src/app/modules/register/register.component.ts
@@ -1,6 +1,9 @@
 // src/app/register/register.component.ts
 
 import { Component } from '@angular/core';
+import { User } from '../../core/models/user.model';
+import { RegisterService } from '../../core/services/register/register.service';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -9,10 +12,22 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
-  constructor() {}
+  user: User = {
+    username: '',
+    email: '',
+    password: '',
+  }
 
-  onSubmit() {
-    // Handle the form submission, e.g., register the user
-    console.log('Form submitted!');
+  constructor(private registerService: RegisterService) {}
+
+  onSubmit(form:NgForm) {
+    if(form.invalid){
+      return;
+    }
+    this.registerService.registerUser(this.user).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
   }
 }
