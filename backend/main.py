6e7c9eb000a22a4f1f5aa1fc4228458d00ac3a90
Zachary <tysonzachary@ufl.edu>
2023-09-04 17:44:17 -0400

    CORS origins

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from db.session import database, engine
 from models.user import Base
 from api.routes import user_registration_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+    "https://localhost:4200",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
