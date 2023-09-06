e78a61dae36300aa552aa188b7946e5c0bb96e31
Zachary <tysonzachary@ufl.edu>
2023-09-05 22:27:53 -0400

    login component

--- frontend/src/app/core/services/login/login.service.ts
+++ frontend/src/app/core/services/login/login.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private API_URL = 'http://localhost:8000';
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  loginUser(user:any){
+    const formData: FormData = new FormData();
+    for (const key in user) {
+      if (user.hasOwnProperty(key)) {
+        formData.append(key, user[key]);
+      }
+    }
+    return this.http.post<any>(this.API_URL+"/login",formData)
+  }
 }
--- frontend/src/app/modules/login/login.component.ts
+++ frontend/src/app/modules/login/login.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
+import {User} from "../../core/models/user.model";
+import {SecurityService} from "../../core/services/security/security.service";
+import {LoginService} from "../../core/services/login/login.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
+
+  user: User = {
+    username: '',
+    password: '',
+  }
+  isSubmittedSuccessfully: boolean = false;
+  isSubmitted: boolean = false;
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  constructor(private loginService: LoginService, private securityService: SecurityService) {
+
+  }
+
+  ngOnInit() {
+    this.loginForm.valueChanges.subscribe(() => {
+      if (this.loginForm == null ||
+        this.loginForm.get('password') == null ||
+        this.loginForm.get('username') == null) {
+        return;
+      }
+      if (!this.loginForm?.get('password')?.value ||
+        !this.loginForm?.get('username')?.value) {
+        return;
+      }
+    });
+  }
+  async onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const formValues = this.loginForm.value;
+    if(formValues.username == undefined || formValues.password == undefined) {
+      return;
+    }
+    const hashedPassword = await this.securityService.hashPassword(formValues.password);
+    const newUser: User = {
+      username: formValues.username,
+      password: hashedPassword,
+    };
+
+    this.loginService.loginUser(newUser).subscribe(response => {
+      this.isSubmitted = true;
+      this.isSubmittedSuccessfully = true;
+      console.log(response);
+      response = JSON.parse(JSON.stringify(response));
+      localStorage.setItem('access_token', `Bearer ${response.access_token}`);
+    }, error => {
+      this.isSubmittedSuccessfully = false;
+      this.isSubmitted = true;
+    });
+  }
 
 }
