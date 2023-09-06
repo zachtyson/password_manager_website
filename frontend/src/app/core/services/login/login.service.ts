a2c27e1b8cf447cc340baa125d45fabefb235c9b
Zachary <tysonzachary@ufl.edu>
2023-09-06 19:55:37 -0400

    changed backend login routing

--- frontend/src/app/core/services/login/login.service.ts
+++ frontend/src/app/core/services/login/login.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {User} from "../../models/user.model";
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,21 @@ export class LoginService {
   private API_URL = 'http://localhost:8000';
   constructor(private http:HttpClient) { }
 
-  loginUser(user:any){
+  loginUser(user: User){
     const formData: FormData = new FormData();
-    for (const key in user) {
-      if (user.hasOwnProperty(key)) {
-        formData.append(key, user[key]);
-      }
+    let loginUrl = this.API_URL+"/login";
+    if (user.username && user.password) {
+      formData.append('username', user.username);
+      formData.append('password', user.password);
+      loginUrl = loginUrl + "/u";
+    } else if (user.email && user.password) {
+      formData.append('email', user.email);
+      formData.append('password', user.password);
+      loginUrl = loginUrl + "/e";
+    } else {
+      throw new Error('Invalid user object. Must contain either username or email along with a password.');
     }
-    return this.http.post<any>(this.API_URL+"/login",formData)
+    return this.http.post<any>(loginUrl,formData)
   }
 
   checkIfUserIsLoggedIn():boolean{
