7fef89933987535363f7239a5c1cdf443460de9e
Zachary <tysonzachary@ufl.edu>
2023-09-07 13:44:04 -0400

    add sidebar

--- frontend/src/app/modules/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/dashboard/dashboard.component.ts
@@ -2,14 +2,28 @@ import { Component } from '@angular/core';
 import {LoginService} from "../../core/services/login/login.service";
 import {SecurityService} from "../../core/services/security/security.service";
 import {Router} from "@angular/router";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('open', style({ left: '0' })),
+      state('closed', style({ left: '-15vw' })),
+      transition('open <=> closed', animate('300ms ease-in-out')),
+    ]),
+  ],
 })
 export class DashboardComponent {
   constructor(private loginService: LoginService, private securityService: SecurityService,private router: Router) {
+  }
+
+  isOpen = true;
+
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
   }
 
   ngOnInit() {
