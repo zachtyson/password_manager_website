529143cb91e1cf08688efdecd77359bc18f666be
Zachary <tysonzachary@ufl.edu>
2023-09-07 13:23:07 -0400

    redirect if not logged in

--- frontend/src/app/modules/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {LoginService} from "../../core/services/login/login.service";
+import {SecurityService} from "../../core/services/security/security.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  constructor(private loginService: LoginService, private securityService: SecurityService,private router: Router) {
+  }
 
+  ngOnInit() {
+    if(!this.loginService.checkIfUserIsLoggedIn()) {
+      //if user is not logged in, redirect to login page
+      this.router.navigate(['/login']);
+    }
+  }
 }
