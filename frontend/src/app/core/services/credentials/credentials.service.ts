ab4d8970531d81730894497cc17de0bec2861783
Zachary <tysonzachary@ufl.edu>
2023-09-08 14:25:56 -0400

    todo

--- frontend/src/app/core/services/credentials/credentials.service.ts
+++ frontend/src/app/core/services/credentials/credentials.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {Credential} from "../../models/saved-credential.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CredentialsService {
+  private API_URL = 'http://localhost:8000';
 
   constructor() { }
+
+  getCredentials(access_token: string): Credential[] {
+    //todo: verify access token once backend is implemented
+    return [];
+  }
 }
