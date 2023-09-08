b01d3597959fcab3c1064ee34ec9ace943616571
Zachary <tysonzachary@ufl.edu>
2023-09-08 14:19:32 -0400

    created saved credential model

---
+++ frontend/src/app/core/models/saved-credential.model.ts
@@ -0,0 +1,14 @@
+import {User} from "./user.model";
+
+export interface Credential {
+  id?: number;
+  owner_id?: number;
+  nickname?: string;
+  username?: string;
+  encrypted_password?: string;
+  email?: string;
+  added_date?: Date;
+  last_accessed_date?: Date;
+  url?: string;
+  shared_users?: User[];
+}
