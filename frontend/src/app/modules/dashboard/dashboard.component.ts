2193d607e5407ed4c533dabefcd78488a07e4ad2
Zachary <tysonzachary@ufl.edu>
2023-09-08 14:43:54 -0400

    gets and prints stored credential in plaintext

--- frontend/src/app/modules/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import {LoginService} from "../../core/services/login/login.service";
 import {SecurityService} from "../../core/services/security/security.service";
 import {Router} from "@angular/router";
 import {animate, state, style, transition, trigger} from "@angular/animations";
+import {CredentialsService} from "../../core/services/credentials/credentials.service";
+import {Credential} from "../../core/models/saved-credential.model";
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +12,7 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  constructor(private loginService: LoginService, private securityService: SecurityService,private router: Router) {
+  constructor(private loginService: LoginService, private securityService: SecurityService,private router: Router, private credentialsService: CredentialsService) {
   }
 
   ngOnInit() {
@@ -18,5 +20,19 @@ export class DashboardComponent {
       //if user is not logged in, redirect to login page
       this.router.navigate(['/login']);
     }
+    const token = localStorage.getItem('access_token');
+    if(token != null) {
+      this.credentialsService.getCredentials(token).subscribe((data: Credential[]) => {
+        this.savedCredentials = data;
+        console.log(this.savedCredentials);
+        for(let i = 0; i < this.savedCredentials.length; i++) {
+          this.stringifiedCredentials.push(JSON.stringify(this.savedCredentials[i]));
+        }
+      }, error => {
+        console.log(error);
+      });
+    }
   }
+  savedCredentials:Credential[] = [];
+  stringifiedCredentials:string[] = [];
 }
