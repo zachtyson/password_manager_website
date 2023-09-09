bf379b12717fcc384220d14fb468006ec8fa7fa6
Zachary <tysonzachary@ufl.edu>
2023-09-09 15:21:29 -0400

    signs out and redirects after 500ms

--- frontend/src/app/modules/signout/signout.component.ts
+++ frontend/src/app/modules/signout/signout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../core/services/auth/auth.service";
 
 @Component({
   selector: 'app-signout',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./signout.component.scss']
 })
 export class SignoutComponent {
+  constructor(private authService: AuthService) {}
 
+  successfulClearLocalStorage: boolean|null = false;
+  ngOnInit() {
+    this.successfulClearLocalStorage = this.authService.clearLocalStorage();
+  }
+  ngAfterViewInit() {
+    setTimeout(() => {
+      window.location.href = '/';
+    }, 500);
+  }
 }
