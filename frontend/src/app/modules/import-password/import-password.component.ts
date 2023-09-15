f8e863901755eabb0f2566e0c99ce3e5644ee4dc
Zachary <tysonzachary@ufl.edu>
2023-09-15 11:56:46 -0400

    button to open file explorer

--- frontend/src/app/modules/import-password/import-password.component.ts
+++ frontend/src/app/modules/import-password/import-password.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./import-password.component.scss']
 })
 export class ImportPasswordComponent {
+  onFileSelected(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
 
+    if (fileInput.files && fileInput.files[0]) {
+      const file = fileInput.files[0];
+      console.log('File selected:', file.name);
+
+      // Process your file here (e.g., read it, parse it, etc.)
+    }
+  }
 }
