1d6bd96c11830424479bef8d942c91f48b517d59
Zachary <tysonzachary@ufl.edu>
2023-09-16 03:35:51 -0400

    added extra check

--- frontend/src/app/modules/credentialcard/credential-card.component.ts
+++ frontend/src/app/modules/credentialcard/credential-card.component.ts
@@ -31,29 +31,61 @@ export class CredentialCardComponent {
     const dialogRef = this.dialog.open(MasterPasswordDialogComponent, {
       data: { masterPassword: '' }
     });
-    let masterPassword: string = '';
+
+
+    if(!this.data?.salt || !this.data?.encrypted_password) {
+      return;
+    }
+
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        masterPassword = result;
-        console.log(masterPassword);
+        const masterPassword = result;
         const salt = this.data?.salt;
-        if(salt == undefined) {
-          return;
-        }
         const encryptedPassword = this.data?.encrypted_password;
-        if(encryptedPassword == undefined) {
+        const credential_id = this.data?.id;  // Assuming your data has an id property for the credential ID.
+
+        if (!salt || !encryptedPassword || !credential_id) {
           return;
         }
-        const decryptedPassword = this.credentialsService.decrypt(encryptedPassword, masterPassword, salt);
-        console.log(decryptedPassword);
-        if(this.data == undefined) {
+
+        const access_token = this.authService.getJwtToken();
+
+        if(!access_token) {
+          // User is not logged in. You can show an error message here if you want.
+          // This shouldn't happen since they would've been redirected to the login page.
+          console.error('User is not logged in');
+          this.showPassword = false;
           return;
         }
-        this.showPassword = true;
-        this.data.encrypted_password = decryptedPassword;
+
+        // Use verifyMasterPassword to check the masterPassword
+        this.credentialsService.verifyMasterPassword(access_token, masterPassword, credential_id)
+          .subscribe(isVerified => {
+            if (isVerified) {
+              const decryptedPassword = this.credentialsService.decrypt(encryptedPassword, masterPassword, salt);
+              if(!decryptedPassword) {
+                // Decryption failed. You can show an error message here if you want.
+                console.error('Decryption failed');
+                this.showPassword = false;
+                return;
+              }
+              if(!this.data) {
+                // This should never happen.
+                console.error('Credential data is null');
+                return;
+              }
+              this.data.encrypted_password = decryptedPassword;  // This line seems incorrect, you might be setting decrypted password to encrypted_password field.
+              this.showPassword = true;
+            } else {
+              // Master password verification failed. You can show an error message here if you want.
+              console.error('Master password verification failed');
+              this.showPassword = false;
+            }
+          });
       } else {
         return;
       }
     });
   }
+
 }
