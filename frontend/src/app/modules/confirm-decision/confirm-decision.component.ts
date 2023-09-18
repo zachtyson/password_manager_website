677244ef3a88295016fc89246f2d74b99e95b6d3
Zachary <tysonzachary@ufl.edu>
2023-09-17 20:06:56 -0400

    confirm component

--- frontend/src/app/modules/confirm-decision/confirm-decision.component.ts
+++ frontend/src/app/modules/confirm-decision/confirm-decision.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {MasterPasswordDialogComponent} from "../master-password-dialog/master-password-dialog.component";
 
 @Component({
   selector: 'app-confirm-decision',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./confirm-decision.component.scss']
 })
 export class ConfirmDecisionComponent {
+  constructor(
+    public dialogRef: MatDialogRef<MasterPasswordDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { confirm: string }
+  ) {}
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
