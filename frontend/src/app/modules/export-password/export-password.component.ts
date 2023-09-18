d39751a971e713c39fa2e85135ec07938425a37d
Zachary <tysonzachary@ufl.edu>
2023-09-17 20:01:02 -0400

    function that prints to console

--- frontend/src/app/modules/export-password/export-password.component.ts
+++ frontend/src/app/modules/export-password/export-password.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {CredentialsService} from "../../core/services/credentials/credentials.service";
+import {AuthService} from "../../core/services/auth/auth.service";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-export-password',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./export-password.component.scss']
 })
 export class ExportPasswordComponent {
+  constructor(private credentialsService: CredentialsService, private authService: AuthService, private dialog: MatDialog) {
+  }
 
+  onExportPassword() {
+    console.log('Exporting password');
+  }
 }
